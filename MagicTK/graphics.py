09f3615aa25e70eeaefe4698feb487f7a8227190
CodeCrafter-TL <1825456084@qq.com>
2024-07-20 21:09:22 +0800

    Update version 0.0.1.alpha2

--- MagicTK/graphics.py
+++ MagicTK/graphics.py
@@ -15,7 +15,7 @@ class rectangle:
         self.width = size[0]
         self.height = size[1]
 
-        self.master.create_rectangle(
+        self.canvasId = self.master.create_rectangle(
             self.x, self.y, self.x+self.width, self.y+self.height, fill=self.fill, outline=self.fill)
 
 
@@ -32,8 +32,25 @@ class perfectCircle:
         self.radius = radius
         self.diameter = self.radius * 2
 
-        self.master.create_oval(
+        self.canvasId = self.master.create_oval(
             self.x, self.y, self.x+self.diameter, self.y+self.diameter, fill=self.fill, outline=self.fill)
+
+
+class ellipse:
+
+    def __init__(self, master: tk.Canvas, position: tuple[int | float, int | float],
+                 size: tuple[int | float, int | float], bg: str) -> None:
+        self.master = master
+
+        self.fill = bg
+
+        self.x = position[0]
+        self.y = position[1]
+        self.width = size[0]
+        self.height = size[1]
+
+        self.canvasId = self.master.create_oval(
+            self.x, self.y, self.x+self.width, self.y+self.height, fill=self.fill, outline=self.fill)
 
 
 class roundedRectangle:
